90836082a89bffeccb3ca3513462a4d9d649f86b
Alstudd <alstonsoares17@gmail.com>
2023-09-14 22:16:08 +0530

    StartYourJourney.jsx file changed

--- src/components/Home/StartYourJourney.jsx
+++ src/components/Home/StartYourJourney.jsx
@@ -5,7 +5,7 @@ export default function StartYourJourney() {
   return (
     <div>
       <div className="flex flex-col gap-10 xl:items-center lg:items-start items-center xl:ml-0 lg:ml-24 ml-0 xl:mr-14 mr-0">
-        <h1 className="text-center lg:text-6xl md:text-5xl text-4xl font-extrabold md:leading-normal leading-tight">
+        <h1 className="text-center lg:text-6xl md:text-5xl text-4xl font-extrabold md:leading-normal leading-tight sm:w-auto w-[21rem]">
           Start Your <span className="gradText2">Journey</span> With Us
         </h1>
         <div className="xl:ml-0 lg:ml-10 ml-0 flex items-center justify-center gap-5 mb-28">
